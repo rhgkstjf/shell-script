82e31c896608e5f8593c635a21421ccc20b4ff05
rhgkstjf <44472886+rhgkstjf@users.noreply.github.com>
2020-10-28 02:21:26 +0900

    Update WebLog-transport.sh

--- WebLog-transport.sh
+++ WebLog-transport.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 declare -a array
-array=$(echo [FolderPath]*)
+array=$(echo [SaveFolderPath]/*)
+P=$(date "+%Y-%m-%d")
+
+ssh [user]@[ip] 'mkdir [SaveFolderPath]$(date "+%Y-%m-%d")'
+ssh [user]@[ip] 'rm [SaveFolderPath]/Week/*'
 
 for A in ${array}; do
         if [[ "$A" =~ "access.log" ]]; then
@@ -9,9 +13,12 @@ for A in ${array}; do
                         continue
                 else
                         echo ${A:20}
-                        echo [passwd] | sudo --stdin scp -rP [port] $A [user]@[ip]:[SaveFolderPath]${A:20}
+                        scp -rP [port] $A [user]@[ip]:[SaveFolderPath]/$(date "+%Y-%m-%d")/${A:20}
+                        scp -rP [port] $A [user]@[ip]:[SaveFolderPath]/${A:20}
+
                 fi
         fi
 done
+
 #txt file 을 만들어 두셔야합니다. 용도는 전송한 날짜 저장을 위함입니다.
-ssh -p [port] [user]@[ip] 'echo $(date "+%Y-%m-%d") >> [Folderpath]/[txt file name]'
+ssh -p [port] [user]@[ip] 'echo $(date "+%Y-%m-%d") >> [Folderpath]/[txt file name'
